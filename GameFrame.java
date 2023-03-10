adfb344637359f24ec25c03257c94439ec0dd9e4
eblits <119444591+eblits@users.noreply.github.com>
2023-03-09 17:02:57 -0800

    GameFrame.java

---
+++ GameFrame.java
@@ -0,0 +1,19 @@
+package snakedemo.snakedemo;
+
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame{
+
+    private static final long serialVersionUID = 1L;
+
+    GameFrame() {
+        GamePanel panel = new GamePanel();
+        this.add(panel);
+        this.setTitle("Snake Game");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+}
